def44ae811ef0235de69d9873dab9825ec406b88
fangwenbo <1121112994@qq.com>
2018-01-05 16:45:48 +0800

    新建的项目

---
+++ src/com/zhiyou100/test/demo01.java
@@ -0,0 +1,15 @@
+package com.zhiyou100.test;
+
+/**  
+* @ClassName: demo01  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:41:39
+*  
+*/
+public class demo01 {
+
+	public static void main(String[] args) {
+		System.out.println(111111111);
+	}
+}
