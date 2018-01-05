3c2e006c6f57911ddbf87c3eb19916d3a01dc7bd
fangwenbo <1121112994@qq.com>
2018-01-05 16:48:31 +0800

    新增一个class

---
+++ src/com/zhiyou100/test/demo02.java
@@ -0,0 +1,15 @@
+
+package com.zhiyou100.test;
+
+/**  
+* @ClassName: demo02  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:46:55
+*  
+*/
+public class demo02 {
+public static void main(String[] args) {
+	System.out.println("新增文件");
+}
+}
